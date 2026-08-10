866a3243585cc66f6d280930eb32ef3c819e7d37
Dmitry-Merzlyakov-2002 <66354385+Dmitry-Merzlyakov-2002@users.noreply.github.com>
2026-08-10 11:14:56 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'v10-height-chain-fix';
+const VERSION = 'v11';
 const APP_SHELL = `ce-shell-${VERSION}`;
 const STATIC_CACHE = `ce-static-${VERSION}`;
 const RUNTIME_CACHE = `ce-runtime-${VERSION}`;
